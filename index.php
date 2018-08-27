80f5506e430dfb16328c2dde9b65de0b61be9147
BernardAngga <42725315+BernardAngga@users.noreply.github.com>
2018-08-27 10:43:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,28 @@
 //tuliskan loopingnya di sini
 // mencoba untuk edit file
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method="POST">
+		<label>Masukkan Angka</label>
+		<input type="text" name="angka">
+		<input type="submit">
+	</form>
+
+	<?php
+	$tes = $_POST["angka"];
+	for($i=0;$i<=$tes;$i++){
+		for($k=$tes-$i;$k>=1;$k--){
+			echo "* ";
+
+		}
+		echo "<br>";
+
+	}
+?>
+</body>
+</html>
